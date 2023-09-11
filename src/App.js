2ac8c0cd73b0a5edb7f589b45db606660fff01c3
Samanyu <72296412+Sam-ops09@users.noreply.github.com>
2023-09-11 11:11:36 +0530

    fourth

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Navbar from './components/Navbar';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar/>
+            </div>
+        )
+    }
+}
